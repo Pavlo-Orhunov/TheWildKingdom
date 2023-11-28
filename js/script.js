b26aa14d594f6cfb4555524430230581a9638596
Pavlo-Orhunov <pavlo.orhunov@gmail.com>
2023-11-28 17:17:56 +0200

    some hovers are modified and popup is added

--- js/script.js
+++ js/script.js
@@ -159,3 +159,148 @@ function documentActions(e) {
   }
 }
 // ------------- END OF smooth scroll-to-section -------------
+
+// ------------- Popup windows -------------
+// Define variables
+const popupLinks = document.querySelectorAll(".popup-link")
+const popupCloseIcon = document.querySelectorAll(".close-popup")
+const lockPadding = document.querySelectorAll(".lock-padding")
+const body = document.querySelector("body")
+const timeout = 500
+
+let unlock = true
+
+if (popupLinks.length > 0) {
+  for (let index = 0; index < popupLinks.length; index++) {
+    const popupLink = popupLinks[index]
+    popupLink.addEventListener("click", function (e) {
+      const popupName = popupLink.getAttribute("href").replace("#", "")
+      const curentPopup = document.getElementById(popupName)
+      popupOpen(curentPopup)
+      e.preventDefault()
+    })
+  }
+}
+
+if (popupCloseIcon.length > 0) {
+  for (let index = 0; index < popupCloseIcon.length; index++) {
+    const el = popupCloseIcon[index]
+    el.addEventListener("click", function (e) {
+      popupClose(el.closest(".popup"))
+      e.preventDefault()
+    })
+  }
+}
+
+function popupOpen(curentPopup) {
+  if (curentPopup && unlock) {
+    const popupActive = document.querySelector(".popup.open")
+    if (popupActive) {
+      popupClose(popupActive, false)
+    } else {
+      bodyLock()
+    }
+    curentPopup.classList.add("open")
+    curentPopup.addEventListener("click", function (e) {
+      if (!e.target.closest(".popup__content")) {
+        popupClose(e.target.closest(".popup"))
+      }
+    })
+  }
+}
+
+function popupClose(popupActive, doUnlock = true) {
+  if (unlock) {
+    const videoContainers = popupActive.querySelectorAll("video")
+    if (videoContainers.length > 0) {
+      videoContainers.forEach((video) => {
+        video.pause()
+      })
+    }
+
+    const iframes = popupActive.querySelectorAll("iframe")
+    iframes.forEach((iframe) => {
+      const src = iframe.getAttribute("src")
+      if (src.includes("youtube.com") || src.includes("player.vimeo.com")) {
+        iframe.src = src
+      }
+    })
+
+    popupActive.classList.remove("open")
+    if (doUnlock) {
+      bodyUnLock()
+    }
+  }
+}
+
+function bodyLock() {
+  const lockPaddingValue =
+    window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px"
+
+  if (lockPadding.length > 0) {
+    for (let index = 0; index < lockPadding.length; index++) {
+      const el = lockPadding[index]
+      el.style.paddingRight = lockPaddingValue
+    }
+  }
+  body.style.paddingRight = lockPaddingValue
+  body.classList.add("body--lock")
+
+  unlock = false
+  setTimeout(function () {
+    unlock = true
+  }, timeout)
+}
+
+function bodyUnLock() {
+  setTimeout(function () {
+    if (lockPadding.length > 0) {
+      for (let index = 0; index < lockPadding.length; index++) {
+        const el = lockPadding[index]
+        el.style.paddingRight = "0px"
+      }
+    }
+    body.style.paddingRight = "0px"
+    body.classList.remove("body--lock")
+  }, timeout)
+
+  unlock = false
+  setTimeout(function () {
+    unlock = true
+  }, timeout)
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" || e.code === "Escape") {
+    const popupActive = document.querySelector(".popup.open")
+    if (popupActive) {
+      popupClose(popupActive)
+    }
+  }
+})
+;(function () {
+  // проверяем поддержку
+  if (!Element.prototype.closest) {
+    // реализуем
+    Element.prototype.closest = function (css) {
+      var node = this
+      while (node) {
+        if (node.matches(css)) return node
+        else node = node.parentElement
+      }
+      return null
+    }
+  }
+})()
+;(function () {
+  // проверяем поддержку
+  if (!Element.prototype.matches) {
+    // определяем свойство
+    Element.prototype.matches =
+      Element.prototype.matchesSelector ||
+      Element.prototype.webkitMatchesSelector ||
+      Element.prototype.mozMatchesSelector ||
+      Element.prototype.msMatchesSelector
+  }
+})()
+// ------------- END OF Popup windows -------------
