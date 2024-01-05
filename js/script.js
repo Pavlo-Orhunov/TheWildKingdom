9aa813d3747286987759182eebb6cb538b56a3e6
pavlo <pavlo.orhunov@gmail.com>
2024-01-05 21:25:57 +0200

    layout images are added

--- js/script.js
+++ js/script.js
@@ -175,8 +175,8 @@ if (popupLinks.length > 0) {
     const popupLink = popupLinks[index]
     popupLink.addEventListener("click", function (e) {
       const popupName = popupLink.getAttribute("href").replace("#", "")
-      const curentPopup = document.getElementById(popupName)
-      popupOpen(curentPopup)
+      const currentPopup = document.getElementById(popupName)
+      popupOpen(currentPopup)
       e.preventDefault()
     })
   }
@@ -192,16 +192,16 @@ if (popupCloseIcon.length > 0) {
   }
 }
 
-function popupOpen(curentPopup) {
-  if (curentPopup && unlock) {
+function popupOpen(currentPopup) {
+  if (currentPopup && unlock) {
     const popupActive = document.querySelector(".popup.open")
     if (popupActive) {
       popupClose(popupActive, false)
     } else {
       bodyLock()
     }
-    curentPopup.classList.add("open")
-    curentPopup.addEventListener("click", function (e) {
+    currentPopup.classList.add("open")
+    currentPopup.addEventListener("click", function (e) {
       if (!e.target.closest(".popup__content")) {
         popupClose(e.target.closest(".popup"))
       }
